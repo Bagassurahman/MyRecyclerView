b30466badb57b9fc08010b60ce5dc01d14fc7138
Bagassurahman <bagassurahman2@gmail.com>
2020-11-09 20:42:45 +0700

    task KK4A

---
+++ app/src/main/java/com/bagas/myrecyclerview/Hero.kt
@@ -0,0 +1,10 @@
+package com.bagas.myrecyclerview
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Hero(
+    var name: String,
+    var description: String,
+    var photo: String
+) :Parcelable
